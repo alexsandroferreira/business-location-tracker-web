aabfe3dc06b494d0e057e228781b102722a80bf1
alexsandroferreira <ferreira.alex@outlook.com>
2024-10-08 23:01:19 -0300

    feat: added cypress

---
+++ cypress.config.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// eslint-disable-next-line prettier/prettier
+
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    video: true,
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/results',
+      overwrite: false,
+      html: true,
+      json: false,
+      timestamp: 'mmddyyyy_HHMMss',
+    },
+  },
+})
